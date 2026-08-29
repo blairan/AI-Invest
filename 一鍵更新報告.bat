1559f0f275144b4b48a145c3bf126bd0875ae6fb
blairan <blairan121122885@gmail.com>
2026-08-29 12:28:41 +0800

    Merge stash

--- 一鍵更新報告.bat
+++ 一鍵更新報告.bat
@@ -1,6 +1,10 @@
 @echo off
 echo 正在為您抓取期交所最新數據，請稍候...
+<<<<<<< Updated upstream
 cd /d "f:\程式設計\AI投資"
+=======
+cd /d "%~dp0"
+>>>>>>> Stashed changes
 python scraper_and_report.py
 echo 更新完成！正在為您開啟報告...
 start daily_report.html
